7f9be97b8c4c0dc00c19900bc97cd1fd2fcf7647
KhallilB <khallil.bailey@gmail.com>
2018-10-12 18:06:15 -0700

    added a controller

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const argv = require('yargs').argv;
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
@@ -10,29 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true, newUrlParser: true }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 
-const request = require('request')
-let apiKey = '8ab2dcf2e766a0cfb3ff7dd4baf947f4';
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.post('/', (req, res) => {
-    let city = argv.c || req.body.city
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-    request(url, (err, res, body) => {
-    if(err){
-        console.log('error:', err);
-    } else {
-        let weather = JSON.parse(body)
-        let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        console.log('body', body)
-        console.log('Message:', message)
-    }
-});
-    res.render('index')
-    console.log(req.body.city)
-})
+require('./controllers/datas')(app);
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
---
+++ controllers/datas.js
@@ -0,0 +1,28 @@
+const argv = require('yargs').argv;
+const request = require('request');
+let apiKey = '8ab2dcf2e766a0cfb3ff7dd4baf947f4';
+
+module.exports = (app) => {
+
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
+    
+    app.post('/', (req, res) => {
+        let city = argv.c || req.body.city
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        request(url, (err, res, body) => {
+        if(err){
+            console.log('error:', err);
+        } else {
+            let weather = JSON.parse(body)
+            let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            console.log('body', body)
+            console.log('Message:', message)
+        }
+    });
+        res.render('index')
+        console.log(req.body.city)
+    })
+
+}
